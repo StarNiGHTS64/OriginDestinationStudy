383494b25331515c80ee72c3d5e2753cc7f2c778
OMEN <carlos.becerrao@jouanen.com.mx>
2023-04-21 18:43:53 -0600

    Add project files.

---
+++ Source/Loader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ICities;
+using UnityEngine;
+using ColossalFramework.Plugins;
+
+namespace ODStudy
+{
+    public class Loader : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            GameObject testGameObject = new GameObject("Test Object");
+            testGameObject.AddComponent<ODStudyBehaviour>();
+        }
+    }
+
+    public class ODStudyBehaviour : MonoBehaviour
+    {
+        void Start()
+        {
+            //DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "IT WORKS!!! OH MY STARS!!!!");
+            Debug.Log("IT WORKS!!! OH MY STARS!!!!");
+        }
+        void Update()
+        {
+
+        }
+    }
+}
