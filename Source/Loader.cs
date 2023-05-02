c1cd918d7293630ae3833b518ea41ee2547b1486
StarNiGHTS <carlos.becerrao@jouanen.com.mx>
2023-05-01 22:11:58 -0600

    All necesary GUI elements added

--- Source/Loader.cs
+++ Source/Loader.cs
@@ -2,6 +2,7 @@
 using ICities;
 using UnityEngine;
 using ColossalFramework.Plugins;
+using ColossalFramework.UI;
 
 namespace ODStudy
 {
@@ -11,6 +12,42 @@ namespace ODStudy
         {
             GameObject testGameObject = new GameObject("Test Object");
             testGameObject.AddComponent<ODStudyBehaviour>();
+
+            //This get the default UIView
+            UIView view = UIView.GetAView();
+
+            //This adds an UIComponent to the view
+
+            //GUI Components from Bus State
+            UIComponent stateUIC = view.AddUIComponent(typeof(StatePanel));
+            UIDragHandle statePanelDrag = (UIDragHandle)stateUIC.AddUIComponent(typeof(UIDragHandle));
+
+            UILabel stateLabel = (UILabel)stateUIC.AddUIComponent(typeof(StateLabel));
+
+            UILabel stateText = (UILabel)stateUIC.AddUIComponent(typeof(StateText));
+            UIButton stateLight = (UIButton)stateUIC.AddUIComponent(typeof(StateLight));
+            UISprite stateSprite = (UISprite)stateUIC.AddUIComponent(typeof(StateSprite));
+            UIButton stateLightHightlight = (UIButton)stateUIC.AddUIComponent(typeof(StateHightLight));
+
+            //GUI Components from Control Panel
+            UIComponent stageUIC = view.AddUIComponent(typeof(StagePanel));
+            UIDragHandle stagePanelDrag = (UIDragHandle)stageUIC.AddUIComponent(typeof(UIDragHandle));
+
+            UILabel stageLabel = (UILabel)stageUIC.AddUIComponent(typeof(StageLabel));
+
+            UIButton spawnbutton = (UIButton)stageUIC.AddUIComponent(typeof(SpawnButton));
+            UIButton clearbutton = (UIButton)stageUIC.AddUIComponent(typeof(ClearButton));
+            UIButton stopbutton = (UIButton)stageUIC.AddUIComponent(typeof(StopButton));
+
+            //GUI Components from Metrics
+            UIComponent attributesUIC = view.AddUIComponent(typeof(AttributesPanel));
+            UIDragHandle attributesPanelDrag = (UIDragHandle)attributesUIC.AddUIComponent(typeof(UIDragHandle));
+
+            UILabel attributesLabel = (UILabel)attributesUIC.AddUIComponent(typeof(AttributesLabel));
+
+            UILabel timerText = (UILabel)attributesUIC.AddUIComponent(typeof(TimerLabel));
+            UILabel timerTime = (UILabel)attributesUIC.AddUIComponent(typeof(TimerTime));
+
         }
     }
 
---
+++ Source/UI/ODStudyUI.cs
@@ -0,0 +1,414 @@
+﻿using System;
+using ICities;
+using UnityEngine;
+using ColossalFramework.Plugins;
+using ColossalFramework.UI;
+
+namespace ODStudy
+{
+    /*----------------------- State -----------------------*/
+    class StatePanel : UIPanel
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.backgroundSprite = "ButtonMenuHovered";
+            this.color = new Color32(220, 220, 220, 220);
+            this.width = 150;
+            this.height = 120;
+            this.position = new Vector3(-930, 85);
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    class StateLabel : UILabel
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.backgroundSprite = "ButtonMenuHovered";
+            this.text = "Estado del Bus";
+            this.width = 190;
+            this.height = 30;
+            this.relativePosition = new Vector3(16, 15);
+
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    class StateText : UILabel
+    {
+        String stateText = " ";
+        bool onGoing;
+
+        public override void Start()
+        {
+            onGoing = true;
+            this.text = stateText;
+            this.height = 30;
+            this.width = 160;
+            this.relativePosition = new Vector3(25, 95);
+        }
+
+        public override void Update()
+        {
+            if (onGoing)
+            {
+                stateText = "En Camino...";
+            }
+            else
+            {
+                stateText = "En Espera";
+            }
+
+            this.text = stateText;
+        }
+    }
+
+    class StateHightLight : UIButton
+    {
+        bool onGoing;
+        public override void Start()
+        {
+            onGoing = true;
+
+            this.isInteractive = true;
+            this.color = new Color32(255, 255, 255, 255);
+            this.height = 60;
+            this.width = 50;
+            this.normalBgSprite = "ToolbarIconGroup1Hovered";
+            this.disabledBgSprite = "ToolbarIconGroup1Hovered";
+            this.hoveredBgSprite = "ToolbarIconGroup1Hovered";
+            this.focusedBgSprite = "ToolbarIconGroup1Hovered";
+            this.pressedBgSprite = "ToolbarIconGroup1Hovered";
+            this.relativePosition = new Vector3(50, 34);
+        }
+
+        public override void Update()
+        {
+            if (onGoing)
+            {
+                this.color = new Color32(255, 255, 255, 255);
+            }
+            else
+            {
+                this.color = new Color32(255, 0, 255, 255);
+            }
+        }
+    }
+
+    class StateLight : UIButton
+    {
+        bool onGoing;
+        public override void Start()
+        {
+            onGoing = true;
+
+            this.isInteractive = true;
+            this.color = new Color32(255, 255, 255, 255);
+            this.height = 50;
+            this.width = 50;
+            this.normalBgSprite = "IconPolicyBaseCircle";
+            this.disabledBgSprite = "IconPolicyBaseCircle";
+            this.hoveredBgSprite = "IconPolicyBaseCircle";
+            this.focusedBgSprite = "IconPolicyBaseCircle";
+            this.pressedBgSprite = "IconPolicyBaseCircle";
+            this.relativePosition = new Vector3(50, 38);
+        }
+
+        public override void Update()
+        {
+            if (onGoing)
+            {
+                this.color = new Color32(0, 255, 255, 255);
+            }
+            else
+            {
+                this.color = new Color32(255, 0, 0, 255);
+            }
+        }
+    }
+
+    class StateSprite : UISprite
+    {
+        public override void Start()
+        {
+
+            this.spriteName = "InfoIconTrafficCongestion";
+            this.height = 30;
+            this.width = 30;
+            this.relativePosition = new Vector3(60, 48);
+        }
+
+        public override void Update()
+        {
+
+        }
+
+    }
+
+    /*----------------------- Stage -----------------------*/
+    class StagePanel : UIPanel
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.backgroundSprite = "ButtonMenu";
+            this.color = new Color32(220, 220, 220, 220);
+            this.width = 190;
+            this.height = 150;
+            this.position = new Vector3(-950, -50);
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    class StageLabel : UILabel
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.backgroundSprite = "ButtonMenu";
+            this.text = "Panel de Control";
+            this.width = 190;
+            this.height = 30;
+            this.relativePosition = new Vector3(27.5f, 15);
+
+        }
+
+        public override void Update()
+        {
+        
+        }
+    }
+
+    class StopButton : UIButton
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.color = new Color32(255, 0, 0, 255);
+
+            //Set the text to show on the button
+            this.text = "Stop";
+
+            // Set the button dimensions
+            this.width = 50;
+            this.height = 50;
+
+            //Style the button
+            this.normalBgSprite = "IconPolicyBaseCircle";
+            this.disabledBgSprite = "IconPolicyBaseCircleDisabled";
+            this.hoveredBgSprite = "IconPolicyBaseCircleFocused";
+            this.focusedBgSprite = "IconPolicyBaseCircleHovered";
+            this.pressedBgSprite = "IconPolicyBaseCirclePressed";
+
+            this.tooltip = "Stop";
+
+            this.textColor = new Color32(255, 255, 255, 255);
+            this.disabledTextColor = new Color32(7, 7, 7, 255);
+            this.hoveredTextColor = new Color32(7, 132, 255, 255);
+            this.focusedTextColor = new Color32(255, 255, 255, 255);
+            this.pressedTextColor = new Color32(30, 30, 44, 255);
+
+
+            //Enable button sounds
+            this.playAudioEvents = true;
+
+            //Place the button
+            //this.transformPosition = new Vector3(-1.65f, 0.97f);
+            this.relativePosition = new Vector3(10, 60);
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    class ClearButton : UIButton
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.color = new Color32(255, 255, 0, 255);
+
+            //Set the text to show on the button
+            this.text = "Clear";
+
+            // Set the button dimensions
+            this.width = 50;
+            this.height = 50;
+
+            //Style the button
+            this.normalBgSprite = "IconPolicyBaseCircle";
+            this.disabledBgSprite = "IconPolicyBaseCircleDisabled";
+            this.hoveredBgSprite = "IconPolicyBaseCircleFocused";
+            this.focusedBgSprite = "IconPolicyBaseCircleHovered";
+            this.pressedBgSprite = "IconPolicyBaseCirclePressed";
+
+            this.tooltip = "Clear";
+
+            this.textColor = new Color32(255, 255, 255, 255);
+            this.disabledTextColor = new Color32(7, 7, 7, 255);
+            this.hoveredTextColor = new Color32(7, 132, 255, 255);
+            this.focusedTextColor = new Color32(255, 255, 255, 255);
+            this.pressedTextColor = new Color32(30, 30, 44, 255);
+
+
+            //Enable button sounds
+            this.playAudioEvents = true;
+
+            //Place the button
+            //this.transformPosition = new Vector3(-1.65f, 0.97f);
+            this.relativePosition = new Vector3(70, 60);
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    class SpawnButton : UIButton
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.color = new Color32(0, 255, 0, 255);
+
+            //Set the text to show on the button
+            this.text = "Start";
+
+            // Set the button dimensions
+            this.width = 50;
+            this.height = 50;
+
+            //Style the button
+            this.normalBgSprite = "IconPolicyBaseCircle";
+            this.disabledBgSprite = "IconPolicyBaseCircleDisabled";
+            this.hoveredBgSprite = "IconPolicyBaseCircleFocused";
+            this.focusedBgSprite = "IconPolicyBaseCircleHovered"; 
+            this.pressedBgSprite = "IconPolicyBaseCirclePressed";
+
+            this.tooltip = "Start";
+
+            this.textColor = new Color32(255, 255, 255, 255);
+            this.disabledTextColor = new Color32(7, 7, 7, 255);
+            this.hoveredTextColor = new Color32(7, 132, 255, 255);
+            this.focusedTextColor = new Color32(255, 255, 255, 255);
+            this.pressedTextColor = new Color32(30, 30, 44, 255);
+
+
+            //Enable button sounds
+            this.playAudioEvents = true;
+
+            //Place the button
+            //this.transformPosition = new Vector3(-1.65f, 0.97f);
+            this.relativePosition = new Vector3(130, 60);
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    /*----------------------- Attributes -----------------------*/
+    class AttributesPanel : UIPanel
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.backgroundSprite = "ButtonMenuDisabled";
+            this.color = new Color32(220, 220, 220, 220);
+            this.width = 380;
+            this.height = 150;
+            this.position = new Vector3(580, 480);
+        }
+
+        public override void Update()
+        {
+
+        }
+
+    }
+
+    class AttributesLabel : UILabel
+    {
+        public override void Start()
+        {
+            this.isInteractive = true;
+            this.backgroundSprite = "ButtonMenuDisabled";
+            this.text = "Metricas";
+            this.width = 190;
+            this.height = 30;
+            this.relativePosition = new Vector3(153, 15);
+
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    class TimerTime : UILabel
+    {
+        int seconds, secondsUnit, secondsDec, minutesUnit, minutesDec;
+
+        public override void Start()
+        {
+            seconds = 0;
+            secondsUnit = 0;
+            secondsDec = 0;
+            minutesUnit = 0;
+            minutesDec = 0;
+
+            this.text = minutesDec.ToString() + minutesUnit.ToString() + ":" + secondsDec.ToString() + secondsUnit.ToString();
+            this.textColor = new Color32(255, 0, 0, 255);
+            this.width = 73;
+            this.height = 18;
+            this.relativePosition = new Vector3(200, 60);
+
+        }
+
+        public override void Update()
+        {
+            this.text = minutesDec.ToString() + minutesUnit.ToString() + ":" + secondsDec.ToString() + secondsUnit.ToString(); ;
+        }
+    }
+
+    class TimerLabel : UILabel
+    {
+
+        public override void Start()
+        {
+
+            this.text = "Tiempo:";
+            this.width = 73;
+            this.height = 18;
+            this.relativePosition = new Vector3(50, 60);
+
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+}
+
+
